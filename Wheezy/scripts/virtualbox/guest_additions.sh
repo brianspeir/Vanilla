4784930e8c28f5297de7e9ec762b4ac49cec0554
Brian Speir <no-reply@brianspeir.com>
2014-02-14 15:26:49 -0800

    Making some changes around here.

--- Wheezy/scripts/virtualbox/guest_additions.sh
+++ Wheezy/scripts/virtualbox/guest_additions.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -x
 
-
 echo "Installing dependencies for VirtualBox guest additions."
 apt-get --assume-yes install dkms bzip2 linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,')
 
 echo "Testing..."
 apt-get --assume-yes install --no-install-recommends libdbus-1-3
 
-#
 echo "Installing VirtualBox Guest Additions."
-mount /media/cdrom1
-yes | sh /media/cdrom1/VBoxLinuxAdditions.run
-umount /media/cdrom1
+mount -o loop /home/$username/VBoxGuestAdditions.iso /mnt
+yes | sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+echo "Cleaning VirtualBox."
+rm /home/$username/VBoxGuestAdditions.iso
