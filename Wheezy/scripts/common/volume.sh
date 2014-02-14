86412b48d8b33b23e2c28b687686b2786cc732ce
Brian Speir <no-reply@brianspeir.com>
2014-02-14 15:27:52 -0800

    And goodbye…

--- Wheezy/scripts/common/volume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# Volume ------------------------------------------------------------------- #
-
-#
-echo "Zeroing out the free space."
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
